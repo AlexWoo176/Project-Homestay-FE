fa33974d53cdeeb5e85db10b6ef1515e92796e7a
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-08-01 13:12:07 +0700

    Add Header

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../../../services/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  username: string;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private auth: AuthenticationService,
+  ) { }
 
   ngOnInit(): void {
+    this.username = localStorage.getItem('currentUser');
   }
 
 }
