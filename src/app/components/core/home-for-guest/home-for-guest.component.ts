e0258f0493e23fea916e48b36208a949ae876734
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-08-02 08:24:48 +0700

    Add Home for Guest

--- src/app/components/core/home-for-guest/home-for-guest.component.ts
+++ src/app/components/core/home-for-guest/home-for-guest.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {OrderHouse} from '../../../model/order-house';
+import {OrderService} from '../../../services/order.service';
 
 @Component({
   selector: 'app-home-for-guest',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeForGuestComponent implements OnInit {
 
-  constructor() { }
+  orders: OrderHouse[];
+  day = 86400 * 1000;
+  now: Date;
+
+  constructor(private orderService: OrderService) {
+  }
 
   ngOnInit(): void {
+    this.orderService.getOrders().subscribe(
+      next => {
+        this.orders = next.data;
+        for (const order of this.orders) {
+          order.checkin = new Date(order.checkin);
+          order.checkout = new Date(order.checkout);
+          this.now = new Date();
+          this.orderService.getHouseOfOrder(order.id).subscribe(
+            next2 => order.house = next2.data
+          );
+        }
+      }
+    );
+  }
+
+  cancelOrder(id: number) {
+    this.orderService.deleteOrder(id).subscribe(
+      next => {
+        if (next.success) {
+          alert(next.message);
+          this.orderService.getOrders().subscribe(
+            next1 => {
+              this.orders = next1.data;
+              for (const order of this.orders) {
+                order.checkin = new Date(order.checkin);
+                order.checkout = new Date(order.checkout);
+                this.now = new Date();
+                this.orderService.getHouseOfOrder(order.id).subscribe(
+                  next2 => order.house = next2.data
+                );
+              }
+            }
+          );
+        } else {
+          alert(next.message);
+        }
+      },
+    );
   }
 
 }
